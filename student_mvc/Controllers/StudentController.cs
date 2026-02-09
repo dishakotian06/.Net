debbf7cd2738b7e13448ab09eeddac07eed5ea8d
Disha <disha09github@gmail.com>
2026-02-09 10:52:54 +0530

    Add files via upload

---
+++ student_mvc/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using student_mvc.Models;
+
+namespace student_mvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student {USN=01,mark1=98,mark2=87,mark3=90},
+                new Student {USN=01,mark1=99,mark2=85,mark3=97},
+                new Student  {USN=01,mark1=78,mark2=83,mark3=94},
+
+            };
+            return View(students);
+        }
+    }
+}
---
+++ student_mvc/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace student_mvc.Models
+{
+    public class Student
+    {
+        public Double USN { get; set; }
+        public double mark1 { get; set; }
+        public double mark2 { get; set; }
+        public double mark3 { get; set; }
+
+    }
+}
