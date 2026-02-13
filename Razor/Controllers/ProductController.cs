502f22bd2d839be27b8f3aca880ed49e93c8b4f5
Disha <disha09github@gmail.com>
2026-02-13 12:51:46 +0530

    Add files via upload

---
+++ Razor/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using razor.Models;
+
+namespace razor.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<product> products = new List<product>
+            {
+                new Models.product {Id= 1, Name= "laptop", Price=55000},
+                new Models.product {Id= 2, Name=" mobile", Price=25000},
+                new Models.product {Id= 3, Name= "Headphones",Price=3000}
+            };
+            return View(products);
+        }
+    }
+}
+
+
